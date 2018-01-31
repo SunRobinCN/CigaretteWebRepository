e0e6656ba036a639c236b0658cbe1305ad1085b5
robin.sun <robin.sun@DLJZ1547>
2018-01-31 15:30:00 +0800

    2

--- CigaretteWebTool/Form1.cs
+++ CigaretteWebTool/Form1.cs
@@ -16,5 +16,10 @@ namespace CigaretteWebTool
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
