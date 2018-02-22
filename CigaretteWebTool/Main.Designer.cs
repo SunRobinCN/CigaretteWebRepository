109307ae6135a6fc7d7c2a803ba867cdf3445103
robin.sun <robin.sun@DLJZ1547>
2018-02-22 15:45:34 +0800

    8

--- CigaretteWebTool/Main.Designer.cs
+++ CigaretteWebTool/Main.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser
@@ -37,22 +40,43 @@
             this.webBrowser.Location = new System.Drawing.Point(0, 0);
             this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(1197, 742);
+            this.webBrowser.Size = new System.Drawing.Size(1306, 780);
             this.webBrowser.TabIndex = 0;
             this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
             this.webBrowser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser_Navigating);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.webBrowser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1306, 780);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 745);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1197, 742);
-            this.Controls.Add(this.webBrowser);
+            this.ClientSize = new System.Drawing.Size(1306, 780);
+            this.Controls.Add(this.panel1);
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -60,6 +84,8 @@
         #endregion
 
         private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CigaretteWebTool/StringUtil.cs
+++ CigaretteWebTool/StringUtil.cs
@@ -16,5 +16,18 @@ namespace CigaretteWebTool
                 "//runfunction7\r\n\r\n//runfunction8\r\n\r\n//runfunction9\r\n\r\n }); \r\n\r\n");
             return builder.ToString();
         }
+
+        public string OnLoadScriptForOuter()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(
+                "//function1\r\n\r\n//function2\r\n\r\n " +
+                "//function3\r\n\r\n//function4\r\n\r\n //function5\r\n\r\n//function6\r\n\r\n  " +
+                "//function7\r\n\r\n//function8\r\n\r\n //function9\r\n\r\n\r\n\r\n" +
+                "//runfunction1\r\n\r\n//runfunction2\r\n\r\n//runfunction3\r\n\r\n" +
+                "//runfunction4\r\n\r\n//runfunction5\r\n\r\n//runfunction6\r\n\r\n" +
+                "//runfunction7\r\n\r\n//runfunction8\r\n\r\n//runfunction9\r\n\r\n");
+            return builder.ToString();
+        }
     }
 }
